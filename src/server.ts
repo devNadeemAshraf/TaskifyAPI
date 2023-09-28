20378868a613dda42ebbac5f75417feac9c45072
Nadeem Ashraf <dev.nad.ash@gmail.com>
2023-09-28 18:32:50 +0530

    Added Route /

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 const PORT = process.env.PORT || 3300;
 
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    status: "OK",
+    message: "Server is running",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on : ${PORT}`);
